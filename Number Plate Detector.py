738e7e72fd2a0fb6640592afd1966d2fd80bfb94
Jamal Ali <65658715+Jamal2022@users.noreply.github.com>
2020-05-30 01:52:39 +0100

    Update Number Plate Detector.py

--- Number Plate Detector.py
+++ Number Plate Detector.py
@@ -6,6 +6,7 @@ frameHeight = 480
 nPlateCascade= cv2.CascadeClassifier("Res/haarcascade_russian_plate_number.xml")
 minArea = 500
 Color = (255,0,255)
+count = 0
 ###########################
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
@@ -24,5 +25,11 @@ while True:
             imgRoi = img[y:y+h,x:x+w]
             cv2.imshow("Roi",imgRoi)
     cv2.imshow("Result", img)
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite("Res/Scan/NoPlate"+str(count)+".jpg",imgRoi)
+        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
+        cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
+        cv2.imshow("Result",img)
+        cv2.waitKey(500)
+        count += 1
+        #break
